c412a34408a6e8ac784ccdb4522ce453689da153
edallade <elarad1200@alumno.ipn.mx>
2020-05-01 21:50:10 -0500

    data en RAM

--- SDEP_parte2/cliente.cpp
+++ SDEP_parte2/cliente.cpp
@@ -16,41 +16,45 @@ using namespace  std;
 
 int main (int argc, char*argv[]){
   if(argc!=3){
-        cout<<"Modo de uso: ./cliente ip numero_registros_a_enviar \n";
+        cout<<"Modo de uso: ./cliente ip nombre_archivo_enviar \n";
         exit(0);
   }
   SocketDatagrama cliente(0);
   PaqueteDatagrama1 p_res(sizeof(int));
   vector<struct registro>registros;
+  vector<struct registro>::iterator it;
   int puerto = 7500;
   int fd_archivo;
-  int n_envios = atoi(argv[2]);
+  //int n_envios = atoi(argv[2]);
   mensaje mjs;
   registro reg_aux;
-  if((fd_archivo=open("7000registros",O_RDONLY))==-1){
+  if((fd_archivo=open(argv[2],O_RDONLY))==-1){
     perror(argv[2]);
     exit(0);
   }
   int id_envio=1;
-  int acr;
-  read(fd_archivo, &reg_aux, sizeof(reg_aux));
-  memcpy(mjs.arguments,(char *)&reg_aux,sizeof(reg_aux));
+  int acr,flag;
+  while((flag =  read(fd_archivo, &reg_aux, sizeof(reg_aux)))>0){
+    registros.push_back(reg_aux);
+  }
+  it = registros.begin();
+  memcpy(mjs.arguments,(char *)&*it,sizeof(reg_aux));
   mjs.requestId = id_envio;
-  for(int x = 1;x<=n_envios;x++){
+  for(it=registros.begin()+1;it!=registros.end();it++){
 
   PaqueteDatagrama1  p((char *)&mjs, sizeof(mjs),argv[1],puerto);
    for(;;){
 
     cliente.envia(p);
     PaqueteDatagrama1  p_recibido (sizeof(mjs));
-    acr = cliente.SetDatagramTimeout(p_recibido,3,0);
+    acr = cliente.SetDatagramTimeout(p_recibido,5,0);
       if(acr!=-1){
         memcpy(&mjs,p_recibido.getData(),p_recibido.getLen());
           
-        if(id_envio>mjs.requestId){
+        if(id_envio>=mjs.requestId){
           id_envio++;
-          read(fd_archivo, &reg_aux, sizeof(reg_aux));
-          memcpy(&mjs.arguments,&reg_aux,sizeof(reg_aux));
+          //read(fd_archivo, &reg_aux, sizeof(reg_aux));
+          memcpy(&mjs.arguments,&*it,sizeof(reg_aux));
           mjs.requestId=id_envio;  
           break;
         }
@@ -64,6 +68,13 @@ int main (int argc, char*argv[]){
   
     
   }
+  vector<struct registro>::reverse_iterator r_it;
+  r_it=registros.rbegin();
+  memcpy(&mjs.arguments,&*r_it,sizeof(reg_aux));
+  id_envio++;
+  mjs.requestId= id_envio;
+  PaqueteDatagrama1 ultimo((char*)&mjs,sizeof(mjs),argv[1],puerto);
+  cliente.envia(ultimo);
   mjs.requestId=-1;
   PaqueteDatagrama1 fin((char*)&mjs,sizeof(mjs),argv[1],puerto);
   cliente.envia(fin);
--- SDEP_parte2/servidor.cpp
+++ SDEP_parte2/servidor.cpp
@@ -17,10 +17,13 @@ int main(int argc, char * argv[]){
     cout<<"modo de uso "<<argv[0]<<" nombre_de_archivo_guardar_datos"<<endl;
     exit(0);
   }
+
   SocketDatagrama servidor(7500);
   int fd_archivo;
   mensaje mjs;
   registro reg_aux;
+  vector<struct registro>registros;
+  vector<struct registro>::iterator it;
   if((fd_archivo=open(argv[1],O_WRONLY|O_CREAT|O_TRUNC,0666))==-1){
       perror(argv[1]);
       exit(0);
@@ -36,7 +39,8 @@ int main(int argc, char * argv[]){
      break;
    if(mjs.requestId>control){
      memcpy(&reg_aux,(char *)&mjs.arguments,sizeof(reg_aux));
-     write(fd_archivo,&reg_aux,sizeof(reg_aux));
+     //write(fd_archivo,&reg_aux,sizeof(reg_aux));
+     registros.push_back(reg_aux);
     mjs.requestId=control;
     control++;
     
@@ -49,6 +53,10 @@ int main(int argc, char * argv[]){
  
     
   }  
+  for(it=registros.begin();it!=registros.end();it++){
+    reg_aux=*it;
+    write(fd_archivo,&reg_aux,sizeof(reg_aux));
+  }
   close(fd_archivo);
     
   
